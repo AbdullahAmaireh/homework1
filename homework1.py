a5c9e9ec8c2ca6268af33a433fa21d9a4b642a49
AbdullahAmaireh <112310853+AbdullahAmaireh@users.noreply.github.com>
2022-09-15 21:53:23 +0300

    python homework

---
+++ homework1.py
@@ -0,0 +1,11 @@
+from cProfile import label
+
+
+drinks = ["Coke", "Dite Coke" ,"Pepsi", "Sprite"]
+size = ["small" , "medium", "larg", "extra larg"]
+drinks.sort()
+size.sort()
+print(drinks)
+print(size )
+label= input("chose your drinks pleas")
+label1=input("chose your size pleas")
